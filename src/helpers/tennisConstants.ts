bbdacbe886b2f78c993adbc974f29c843a70e1cf
Mena <mena@vouch.co.uk>
2022-09-05 12:36:24 +0100

    Add Tennis constant arrays

---
+++ src/helpers/tennisConstants.ts
@@ -0,0 +1,18 @@
+
+export const serveOptions = [
+  "Ace",
+  "Immediate Point",
+  "Good Serve",
+  "Fault",
+  "Let",
+];
+
+export const returnOptions = ["Didn't Reach", "Out", "Smesh Ball", "Dink Ball"];
+
+export const playOptions = [
+  "back to opponent",
+  "loose point",
+  "point",
+  "serveAgain",
+  "play",
+];
