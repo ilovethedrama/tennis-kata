f8704b69f5740f4c7788384bf99b0ac9ea012fd5
Mena <75310449+menavouch@users.noreply.github.com>
2022-09-28 11:48:34 +0100

    Add types

---
+++ src/types/game.ts
@@ -0,0 +1,17 @@
+type Set = 0 | 1 | 2 | 3;
+
+type Game = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
+
+export type Point = 0 | 15 | 30 | 40 | "Advantage" | "-";
+
+export type Player = 1 | 2;
+
+export interface GameState {
+  sets: [Set, Set];
+  games: [Game, Game];
+  points: [Point, Point];
+}
+
+export interface PointsState {
+  points: [Point, Point];
+}
