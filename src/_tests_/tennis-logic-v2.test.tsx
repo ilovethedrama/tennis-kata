c9c9aa77af233b9b0811de6a09c3c3a665c9b846
Mena <75310449+menavouch@users.noreply.github.com>
2022-10-06 12:08:03 +0100

    Handle player one winning a second point when the game score starts at 15

--- src/_tests_/tennis-logic-v2.test.tsx
+++ src/_tests_/tennis-logic-v2.test.tsx
@@ -8,5 +8,10 @@ describe("Tennis game", () => {
       const playerOneScore = playerWinsPoint(playerOne);
       expect(playerOneScore.points).toEqual(15);
     });
+    it("handle player one winning a second point when the game score starts at 15", () => {
+      const playerOne = { points: 15, games: 0, sets: 0, matchesWon: 0 };
+      const playerOneScore = playerWinsPoint(playerOne);
+      expect(playerOneScore.points).toEqual(30);
+    });
   });
 });
--- src/helpers/helpers-v2.ts
+++ src/helpers/helpers-v2.ts
@@ -2,5 +2,6 @@ import { playerState } from "../types/playerState";
 
 export function playerWinsPoint(player: playerState) {
   if (player.points === 0) return { ...player, points: 15 };
+  if (player.points === 15) return { ...player, points: 30 };
   return player;
 }
