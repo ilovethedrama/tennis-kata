c948261f03679446401056b305ae81f1668f245c
Mena <75310449+menavouch@users.noreply.github.com>
2022-10-06 11:48:26 +0100

    Refactor

--- src/helpers/helpers-v2.ts
+++ src/helpers/helpers-v2.ts
@@ -1,3 +1,6 @@
-export function playerWinsPoint(player: any) {
+import { playerState } from "../types/playerState";
+
+export function playerWinsPoint(player: playerState) {
   if (player.points === 0) return { ...player, points: 15 };
+  return player;
 }
---
+++ src/types/playerState.ts
@@ -0,0 +1,6 @@
+export interface playerState {
+    points: number;
+    games: number;
+    sets: number;
+    matchesWon: number;
+}
