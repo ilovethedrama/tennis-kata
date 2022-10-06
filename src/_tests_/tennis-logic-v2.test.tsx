999436dcbc12f53a3b2f251582f9cf5d676cc844
Mena <75310449+menavouch@users.noreply.github.com>
2022-10-06 12:11:57 +0100

    Handle player one winning a third point when the game score starts at 30

--- src/_tests_/tennis-logic-v2.test.tsx
+++ src/_tests_/tennis-logic-v2.test.tsx
@@ -13,5 +13,10 @@ describe("Tennis game", () => {
       const playerOneScore = playerWinsPoint(playerOne);
       expect(playerOneScore.points).toEqual(30);
     });
+    it("handle player one winning a third point when the game score starts at 30", () => {
+      const playerOne = { points: 30, games: 0, sets: 0, matchesWon: 0 };
+      const playerOneScore = playerWinsPoint(playerOne);
+      expect(playerOneScore.points).toEqual(40);
+    });
   });
 });
--- src/helpers/helpers-v2.ts
+++ src/helpers/helpers-v2.ts
@@ -3,5 +3,6 @@ import { playerState } from "../types/playerState";
 export function playerWinsPoint(player: playerState) {
   if (player.points === 0) return { ...player, points: 15 };
   if (player.points === 15) return { ...player, points: 30 };
+  if (player.points === 30) return { ...player, points: 40 };
   return player;
 }
