65499db60ccdbea16e8ba8b1bfbdf17d0358f16d
Mena <75310449+menavouch@users.noreply.github.com>
2022-10-06 15:00:33 +0100

    Refactor

--- src/_tests_/tennis-logic-v2.test.tsx
+++ src/_tests_/tennis-logic-v2.test.tsx
@@ -4,25 +4,33 @@ import { playerState } from "../types/playerState";
 
 describe("Tennis game", () => {
   describe("Player One", () => {
-    it("handle player one winning a point when the game starts at 0", () => {
+    it("handle player one winning a point when the game starts at 0-0", () => {
       const playerOne: playerState = { points: 0, games: 0, sets: 0, matchesWon: 0 };
-      const playerOneScore = playerWinsPoint(playerOne);
-      expect(playerOneScore.points).toEqual(15);
+      const playerTwo: playerState = { points: 0, games: 0, sets: 0, matchesWon: 0 };
+      const game = { playerOne, playerTwo}
+      const gameScore = playerWinsPoint(game.playerOne, "Player One");
+      expect(gameScore.points).toEqual(15);
     });
-    it("handle player one winning a second point when the game score is at 15", () => {
+    it("handle player one winning a second point when the game score is at 15-0", () => {
       const playerOne: playerState = { points: 15, games: 0, sets: 0, matchesWon: 0 };
-      const playerOneScore = playerWinsPoint(playerOne);
-      expect(playerOneScore.points).toEqual(30);
+      const playerTwo: playerState = { points: 0, games: 0, sets: 0, matchesWon: 0 };
+      const game = { playerOne, playerTwo}
+      const gameScore = playerWinsPoint(game.playerOne, "Player One");
+      expect(gameScore.points).toEqual(30);
     });
-    it("handle player one winning a third point when the game score is at 30", () => {
+    it("handle player one winning a third point when the game score is at 30-0", () => {
       const playerOne: playerState = { points: 30, games: 0, sets: 0, matchesWon: 0 };
-      const playerOneScore = playerWinsPoint(playerOne);
-      expect(playerOneScore.points).toEqual(40);
+      const playerTwo: playerState = { points: 0, games: 0, sets: 0, matchesWon: 0 };
+      const game = { playerOne, playerTwo}
+      const gameScore = playerWinsPoint(game.playerOne, "Player One");
+      expect(gameScore.points).toEqual(40);
     });
     it("handle player one winning a advantage point when the game score is at -", () => {
-      const playerOne: playerState = { points: "-", games: 0, sets: 0, matchesWon: 0 };
-      const playerOneScore = playerWinsPoint(playerOne);
-      expect(playerOneScore.points).toEqual("Adv");
+      const playerOne: playerState = { points: '-', games: 0, sets: 0, matchesWon: 0 };
+      const playerTwo: playerState = { points: 0, games: 0, sets: 0, matchesWon: 0 };
+      const game = { playerOne, playerTwo}
+      const gameScore = playerWinsPoint(game.playerOne, "Player One");
+      expect(gameScore.points).toEqual("Adv");
     });
   });
 });
--- src/helpers/helpers-v2.ts
+++ src/helpers/helpers-v2.ts
@@ -1,16 +1,20 @@
 import { playerState } from "../types/playerState";
 
-export function playerWinsPoint(player: playerState) {
-  switch (player.points) {
-    case 0:
-      return { ...player, points: 15 };
-    case 15:
-      return { ...player, points: 30 };
-    case 30:
-      return { ...player, points: 40 };
-    case '-':
-      return { ...player, points: 'Adv' };
-      default:
-        return player;
-  }
+export function playerWinsPoint(score: playerState, currentPlayer: string) {
+  const currentScorer = currentPlayer;
+  switch (currentScorer) {
+    case "Player One":
+      switch (score.points) {
+        case 0:
+          return { ...score, points: 15 };
+        case 15:
+          return { ...score, points: 30 };
+        case 30:
+          return { ...score, points: 40 };
+        case "-":
+          return { ...score, points: "Adv" };
+      }
+      break;
+    }
+      return score;
 }
